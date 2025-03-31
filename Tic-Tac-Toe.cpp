e5ac28ddc148b3610cc1057c06e6992f249fac05
Brian <bw49078@gmail.com>
2025-03-30 21:44:57 -0500

    Prompts user for a number, 1 or 2, to pick heads or tails. Added restrictions so user can only enter an integer.

--- Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string input;
+    bool goodRead = false;
+    int num;
+
+    std::cout << "Choose Heads or Tails to see who's going first!\n";
+    std::cout << "1) Heads\n2) Tails\n";
+    
+    while (!goodRead || num > 2 || num < 1)
+    {
+        try
+        {
+            std::cout << "Choice: ";
+            getline(std::cin, input);
+            num = stoi(input);
+            goodRead = true;
+        }
+        catch (...)
+        {
+            goodRead = false;
+        }
+    }
+
+    std::cout << num;
 }
 
